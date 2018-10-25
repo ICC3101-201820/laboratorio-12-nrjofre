f49c5ceeb5532a1c74d439424026e047015436b6
nrjofre <43624060+nrjofre@users.noreply.github.com>
2018-10-25 17:13:30 -0300

    Update MVC/Form1.Designer.cs

--- MVC/Form1.Designer.cs
+++ MVC/Form1.Designer.cs
@@ -169,7 +169,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(761, 312);
             this.Controls.Add(this.EliminarBtn);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.ComidasList);
